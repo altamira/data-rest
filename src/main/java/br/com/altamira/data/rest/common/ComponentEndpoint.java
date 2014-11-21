6237202e49b9a6a188116a3c1e15c526448818f8
Alessandro Holanda <alessandro.holanda@hotmail.com>
2014-11-21 18:33:32 -0200

    add material and component

---
+++ src/main/java/br/com/altamira/data/rest/common/ComponentEndpoint.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.altamira.data.rest.common;
+
+import br.com.altamira.data.rest.BaseEndpoint;
+import javax.enterprise.context.RequestScoped;
+import javax.ws.rs.Path;
+
+/**
+ *
+ * @author Alessandro
+ */
+@RequestScoped
+@Path("common/material/{parentId:[9-0]*}/component")
+public class ComponentEndpoint extends BaseEndpoint<br.com.altamira.data.model.common.Component> {
+    
+}
