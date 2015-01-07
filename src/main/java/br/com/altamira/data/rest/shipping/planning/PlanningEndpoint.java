5e4db8eb3c8736d4d21d2dbc6e0cff6819d23ecf
Alessandro Holanda <alessandro.holanda@hotmail.com>
2015-01-07 16:20:00 -0200

    added shipping planning

---
+++ src/main/java/br/com/altamira/data/rest/shipping/planning/PlanningEndpoint.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.altamira.data.rest.shipping.planning;
+
+import br.com.altamira.data.model.shipping.planning.Planning;
+import br.com.altamira.data.rest.BaseEndpoint;
+import javax.enterprise.context.RequestScoped;
+import javax.ws.rs.Path;
+
+/**
+ *
+ * @author Alessandro
+ */
+@RequestScoped
+@Path("shipping/planning")
+public class PlanningEndpoint extends BaseEndpoint<Planning> {
+
+}
+
