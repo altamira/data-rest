09fc765bb9958eafbd61b82229161407466c39e7
Alessandro Holanda <alessandro.holanda@hotmail.com>
2015-01-14 17:19:27 -0200

    add operation calcule

--- src/main/java/br/com/altamira/data/rest/manufacture/process/OperationEndpoint.java
+++ src/main/java/br/com/altamira/data/rest/manufacture/process/OperationEndpoint.java
@@ -1,9 +1,22 @@
 package br.com.altamira.data.rest.manufacture.process;
 
+import br.com.altamira.data.dao.manufacture.process.OperationDao;
 import javax.ws.rs.Path;
 import br.com.altamira.data.model.manufacture.process.Operation;
+import br.com.altamira.data.model.measurement.Measure;
 import br.com.altamira.data.rest.BaseEndpoint;
+import static br.com.altamira.data.rest.BaseEndpoint.ID_VALIDATION;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import java.util.Map;
 import javax.enterprise.context.RequestScoped;
+import javax.validation.constraints.Min;
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.OPTIONS;
+import javax.ws.rs.POST;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 /**
  *
@@ -13,4 +26,64 @@ import javax.enterprise.context.RequestScoped;
 @Path("manufacturing/process/{parentId:[0-9]*}/operation")
 public class OperationEndpoint extends BaseEndpoint<Operation> {
 
+    /**
+     *
+     * @param id
+     * @param variable
+     * @return
+     * @throws JsonProcessingException
+     */
+    @POST
+    @Path(value = "/{id:[0-9]*}/calcule")
+    @Produces(value = MediaType.APPLICATION_JSON)
+    public Response calcule(
+            @Min(value = 0, message = ID_VALIDATION) @PathParam(value = "id") long id,
+            Map<String, Measure> variable)
+            throws JsonProcessingException {
+
+        return createEntityResponse(
+                ((OperationDao) dao).calcule(
+                        ((OperationDao) dao).find(id), 
+                        variable, 
+                        info.getPathParameters()
+                )
+        ).build();
+    }
+    
+        /**
+     *
+     * @param id
+     * @param variable
+     * @return
+     * @throws JsonProcessingException
+     */
+    @POST
+    @Path(value = "/{id:[0-9]*}/variable")
+    @Produces(value = MediaType.APPLICATION_JSON)
+    public Response variable(
+            @Min(value = 0, message = ID_VALIDATION) @PathParam(value = "id") long id,
+            Map<String, Measure> variable)
+            throws JsonProcessingException {
+
+        ((OperationDao) dao).calcule(
+                        ((OperationDao) dao).find(id), 
+                        variable, 
+                        info.getPathParameters());
+                
+        return createEntityResponse(
+                ((OperationDao) dao).getVariable()
+        ).build();
+    }
+    
+    @OPTIONS
+    @Path("/{id:[0-9]*}/calcule")
+    public Response corsPreflightForCheckedPath(@HeaderParam("Origin") String origin, @PathParam("id") long id) {
+        return getCORSHeaders(origin);
+    }
+
+    @OPTIONS
+    @Path("/{id:[0-9]*}/variable")
+    public Response corsPreflightForUncheckedPath(@HeaderParam("Origin") String origin, @PathParam("id") long id) {
+        return getCORSHeaders(origin);
+    }
 }
--- src/main/java/br/com/altamira/data/rest/manufacture/process/ProduceEndpoint.java
+++ src/main/java/br/com/altamira/data/rest/manufacture/process/ProduceEndpoint.java
@@ -5,23 +5,10 @@
  */
 package br.com.altamira.data.rest.manufacture.process;
 
-import br.com.altamira.data.dao.manufacture.process.OperationDao;
-import br.com.altamira.data.dao.manufacture.process.ProduceDao;
 import br.com.altamira.data.model.manufacture.process.Produce;
-import br.com.altamira.data.model.measurement.Measure;
 import br.com.altamira.data.rest.BaseEndpoint;
-import static br.com.altamira.data.rest.BaseEndpoint.ID_VALIDATION;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import java.util.Map;
-import javax.ejb.EJB;
 import javax.enterprise.context.RequestScoped;
-import javax.validation.constraints.Min;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 
 /**
  *
@@ -31,28 +18,4 @@ import javax.ws.rs.core.Response;
 @Path("manufacturing/process/{process:[0-9]*}/operation/{parentId:[0-9]*}/produce")
 public class ProduceEndpoint extends BaseEndpoint<Produce> {
 
-    @EJB
-    OperationDao operationDao;
-
-    /**
-     *
-     * @param id
-     * @param variables
-     * @return
-     * @throws JsonProcessingException
-     */
-    @POST
-    @Path(value = "/{id:[0-9]*}/simulate")
-    @Produces(value = MediaType.APPLICATION_JSON)
-    public Response simulate(
-            @Min(value = 0, message = ID_VALIDATION) @PathParam(value = "id") long id,
-            Map<String, Measure> variables)
-            throws JsonProcessingException {
-
-        Produce produce = dao.find(id);
-
-        return createEntityResponse(
-                ((ProduceDao) dao).calcule(operationDao.find(
-                                Long.parseLong(info.getPathParameters().get("parentId").get(0))), variables, info.getPathParameters())).build();
-    }
 }
