bf780a369af41e00782c1cbfbc81cd3f11998bf9
Alessandro Holanda <alessandro.holanda@hotmail.com>
2015-01-22 18:28:20 -0200

    add shipping.execution.Delivered entity, add remaining transient field in shipping.planning.delivery entity

---
+++ src/main/java/br/com/altamira/data/model/shipping/execution/DeliveredEndpoint.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.altamira.data.model.shipping.execution;
+
+import br.com.altamira.data.rest.BaseEndpoint;
+import javax.enterprise.context.RequestScoped;
+import javax.ws.rs.Path;
+
+/**
+ *
+ * @author Alessandro
+ */
+@RequestScoped
+@Path("/shipping/execution/delivered")
+public class DeliveredEndpoint extends BaseEndpoint<Delivered> {
+    
+}
