2cede1e0dac370e2f9533b73ecbc0eea9114fb8f
Alessandro Holanda <alessandro.holanda@hotmail.com>
2015-02-19 17:38:08 -0200

    ALTAMIRA-82: add a new api shipping/planning/remaining and shipping/execution/remaining to lazy load the client grid. Change the shipping/planning and shipping/execution to get only the remaining orders list

--- src/main/java/br/com/altamira/data/rest/shipping/execution/BOMEndpoint.java
+++ src/main/java/br/com/altamira/data/rest/shipping/execution/BOMEndpoint.java
@@ -5,10 +5,19 @@
  */
 package br.com.altamira.data.rest.shipping.execution;
 
+import br.com.altamira.data.dao.shipping.execution.BOMDao;
 import br.com.altamira.data.model.shipping.execution.BOM;
 import br.com.altamira.data.rest.BaseEndpoint;
+import java.io.IOException;
 import javax.enterprise.context.RequestScoped;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.core.Response;
 
 /**
  *
@@ -17,5 +26,26 @@ import javax.ws.rs.Path;
 @RequestScoped
 @Path("/shipping/execution")
 public class BOMEndpoint extends BaseEndpoint<BOM> /*implements Endpoint<Process> See https://issues.jboss.org/browse/WFLY-2724*/ {
+    /**
+     *
+     * @param startPosition
+     * @param maxResult
+     * @return
+     * @throws IOException
+     */
+    @GET
+    @Path("/remaining")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response listRemaining(
+            @DefaultValue("0") @QueryParam("start") Integer startPosition,
+            @DefaultValue("10") @QueryParam("max") Integer maxResult)
+            throws IOException {
 
+        MultivaluedMap<String, String> map = info.getPathParameters();
+        
+        map.putAll(info.getQueryParameters());
+        
+        return createListResponse(
+                ((BOMDao) dao).listRemaining(map, startPosition, maxResult)).build();
+    }
 }
--- src/main/java/br/com/altamira/data/rest/shipping/planning/BOMEndpoint.java
+++ src/main/java/br/com/altamira/data/rest/shipping/planning/BOMEndpoint.java
@@ -10,16 +10,22 @@ import br.com.altamira.data.model.shipping.planning.BOM;
 import br.com.altamira.data.rest.BaseEndpoint;
 import static br.com.altamira.data.rest.BaseEndpoint.ID_VALIDATION;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import java.io.IOException;
 import java.util.Date;
 import java.util.List;
 import javax.enterprise.context.RequestScoped;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.Response;
 
 /**
@@ -51,4 +57,26 @@ public class BOMEndpoint extends BaseEndpoint<BOM> /*implements Endpoint<Process
         return createNoContentResponse().build();
     }
 
+    /**
+     *
+     * @param startPosition
+     * @param maxResult
+     * @return
+     * @throws IOException
+     */
+    @GET
+    @Path("/remaining")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response listRemaining(
+            @DefaultValue("0") @QueryParam("start") Integer startPosition,
+            @DefaultValue("10") @QueryParam("max") Integer maxResult)
+            throws IOException {
+
+        MultivaluedMap<String, String> map = info.getPathParameters();
+        
+        map.putAll(info.getQueryParameters());
+        
+        return createListResponse(
+                ((BOMDao) dao).listRemaining(map, startPosition, maxResult)).build();
+    }
 }
