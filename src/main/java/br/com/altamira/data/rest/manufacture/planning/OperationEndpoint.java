43e947dfbf4bca87ec4ad55d0c7eafc372fb72a1
parthpatel-tatvasoft <parth.patel@tatvasoft.com>
2015-03-28 15:02:57 +0530

    ALTAMIRA-159: Manufacture Planning - list BOM's Item

--- src/main/java/br/com/altamira/data/rest/manufacture/planning/OperationEndpoint.java
+++ src/main/java/br/com/altamira/data/rest/manufacture/planning/OperationEndpoint.java
@@ -8,7 +8,9 @@ package br.com.altamira.data.rest.manufacture.planning;
 import br.com.altamira.data.dao.manufacture.planning.OperationDao;
 import br.com.altamira.data.model.manufacture.planning.Operation;
 import br.com.altamira.data.rest.BaseEndpoint;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
+
 import javax.enterprise.context.RequestScoped;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DefaultValue;
@@ -17,6 +19,7 @@ import javax.ws.rs.HeaderParam;
 import javax.ws.rs.OPTIONS;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.MultivaluedMap;
@@ -59,4 +62,26 @@ public class OperationEndpoint extends BaseEndpoint<Operation> {
         //((BOMDao) dao).replaceRemainingDeliveryDates(id, dates);
         return createListResponse(((OperationDao) dao).listBOM(map, startPosition, maxResult)).build();
     }    
+    
+    /**
+     *
+     * @param startPosition
+     * @param maxResult
+     * @return
+     * @throws JsonProcessingException
+     */
+    @GET
+    @Path("/{id:[0-9]*}/bom/{id:[0-9]*}/item")
+    @Produces(value = MediaType.APPLICATION_JSON)
+    public Response ListItem(
+            @DefaultValue("0") @QueryParam("start") Integer startPosition,
+            @DefaultValue("10") @QueryParam("max") Integer maxResult)
+            throws JsonProcessingException {
+
+        MultivaluedMap<String, String> map = info.getPathParameters();
+
+        map.putAll(info.getQueryParameters());
+        
+        return createListResponse(((OperationDao) dao).listBOMItem(map, startPosition, maxResult)).build();
+    }
 }
