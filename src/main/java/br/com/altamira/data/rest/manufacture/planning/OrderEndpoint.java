4bca3c461eddd4f99f069807bd24c8bbca135401
parthpatel-tatvasoft <parth.patel@tatvasoft.com>
2015-04-08 18:46:05 +0530

    ALTAMIRA-175: Manufacture Planning - list order's operations API

--- src/main/java/br/com/altamira/data/rest/manufacture/planning/OrderEndpoint.java
+++ src/main/java/br/com/altamira/data/rest/manufacture/planning/OrderEndpoint.java
@@ -5,10 +5,21 @@
  */
 package br.com.altamira.data.rest.manufacture.planning;
 
+import br.com.altamira.data.dao.manufacture.planning.OrderDao;
 import br.com.altamira.data.model.manufacture.planning.Order;
 import br.com.altamira.data.rest.BaseEndpoint;
+
 import javax.enterprise.context.RequestScoped;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.core.Response;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
 
 /**
  *
@@ -17,5 +28,28 @@ import javax.ws.rs.Path;
 @RequestScoped
 @Path("/manufacture/planning")
 public class OrderEndpoint extends BaseEndpoint<Order> {
+	
+	//ALTAMIRA-175 : Manufacture Planning - create list order's operations API
+	/**
+	 *
+	 * @param startPosition
+	 * @param maxResult
+	 * @return
+	 * @throws JsonProcessingException
+	 */
+	@GET
+	@Path("/{id:[0-9]*}/operation")
+	@Produces(value = MediaType.APPLICATION_JSON)
+	public Response ListOperation(
+			@DefaultValue("0") @QueryParam("start") Integer startPosition,
+			@DefaultValue("10") @QueryParam("max") Integer maxResult)
+					throws JsonProcessingException {
+
+		MultivaluedMap<String, String> map = info.getPathParameters();
+
+		map.putAll(info.getQueryParameters());
+
+		return createEntityResponse(((OrderDao) dao).listOperation(map, startPosition, maxResult)).build();
+	}
 
 }
